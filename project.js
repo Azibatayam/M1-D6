30f0ca34b32c8fbc9670df0f6d6b75cc1e0533d5
Azibatayam <95300506+Azibatayam@users.noreply.github.com>
2021-12-06 14:49:45 +0100

    Exercise 1-2

---
+++ project.js
@@ -0,0 +1,144 @@
+
+// JS Basics
+//* EXERCISE A
+   //Create a variable called test and assign a string value to it.
+
+console.log(test)
+
+let test = {
+    group: "B",
+}
+
+console.log(test)
+
+/* EXERCISE B
+    Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
+*/
+
+let sum = {
+    result: "15",
+
+
+}
+
+console.log(sum)
+
+/* EXERCISE C
+    Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+*/
+
+console.log(random)
+let random = [ 
+    numbers:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,  20 ]
+ ]   
+console.log(numbers)
+
+/* EXERCISE D
+    Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
+*/
+
+let me = {
+    name: "Azibatayam"
+    surname: "Iruo"
+    age:29,
+
+} 
+
+console.log(me)
+
+
+/* EXERCISE E
+    Write a piece of code for programmatically removing the age property from the previously create object.
+*/
+
+delete me.age
+console.log(me)
+    
+/* EXERCISE F
+   Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
+*/
+
+console.log
+
+me.hasSkill = javaScript 
+me.hasSkill = HTML
+me.hasSkill = CSS
+console.log(me)
+
+/* EXERCISE G
+   Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
+*/
+
+delete me.hasSkill = CSS
+console.log(me)
+
+// JS Functions
+/* EXERCISE 1
+    Write a function called dice; it should randomize an integer number between 1 and 6.
+*/
+console.log
+
+function dice = number.slice(1,2,3,4,5,6 ) { 
+}
+    console.log(dice)
+
+
+
+/* EXERCISE 2
+    Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+*/
+
+
+function whoIsBigger (5,6)
+return 6
+
+console.log(whoIsBigger(5,6))
+
+/* EXERCISE 3
+    Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+    Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+*/
+ 
+
+let splitMe = (theFuture) {
+return "the", "Future" 
+
+console.log(splitMe)
+
+
+/* EXERCISE 4
+    Write a function called deleteOne which receives a string and a boolean as parameters.
+    If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+*/
+
+let deleteOne = { 
+             "cup" 
+             true,
+console.log(deleteOne)
+
+
+/* EXERCISE 5
+   Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
+   Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
+*/
+
+let onlyLetters = "made for 1"
+
+for (let 1 = 0;) {
+
+let onlyLetters = made for (1)
+
+console.log(onlyletters)
+}
+
+/* EXERCISE 6
+   Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
+*/
+
+function isThisAnEmail = "welcome"
+
+if isThisAnEmail (welcome)
+
+/* EXERCISE 7
+   Write a function called whatDayIsIt that should return the current day of the week.
+*/
