6e49e63c7f96852204792344d52005684ed2dbc3
Azibatayam <95300506+Azibatayam@users.noreply.github.com>
2021-12-06 16:12:42 +0100

    Exercise up

--- project.js
+++ project.js
@@ -3,7 +3,7 @@
 //* EXERCISE A
    //Create a variable called test and assign a string value to it.
 
-console.log(test)
+//console.log(test)
 
 let test = {
     group: "B",
@@ -27,19 +27,21 @@ console.log(sum)
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
+
+let random =  Math.ceil(Math.random() * 20)
+    
+    
 console.log(random)
-let random = [ 
-    numbers:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,  20 ]
- ]   
-console.log(numbers)
 
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
 
+
+
 let me = {
-    name: "Azibatayam"
-    surname: "Iruo"
+    name: "Azibatayam",
+    surname: "Iruo",
     age:29,
 
 } 
@@ -47,16 +49,16 @@ let me = {
 console.log(me)
 
 
-/* EXERCISE E
-    Write a piece of code for programmatically removing the age property from the previously create object.
-*/
+// /* EXERCISE E
+//     Write a piece of code for programmatically removing the age property from the previously create object.
+// */
 
 delete me.age
 console.log(me)
     
-/* EXERCISE F
-   Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
-*/
+// /* EXERCISE F
+//    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
+// */
 
 console.log
 
@@ -65,17 +67,17 @@ me.hasSkill = HTML
 me.hasSkill = CSS
 console.log(me)
 
-/* EXERCISE G
-   Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
-*/
+// /* EXERCISE G
+//    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
+// */
 
 delete me.hasSkill = CSS
 console.log(me)
 
-// JS Functions
-/* EXERCISE 1
-    Write a function called dice; it should randomize an integer number between 1 and 6.
-*/
+// // JS Functions
+// /* EXERCISE 1
+//     Write a function called dice; it should randomize an integer number between 1 and 6.
+// */
 console.log
 
 function dice = number.slice(1,2,3,4,5,6 ) { 
@@ -84,9 +86,9 @@ function dice = number.slice(1,2,3,4,5,6 ) {
 
 
 
-/* EXERCISE 2
-    Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
-*/
+// /* EXERCISE 2
+//     Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+// */
 
 
 function whoIsBigger (5,6)
@@ -94,33 +96,34 @@ return 6
 
 console.log(whoIsBigger(5,6))
 
-/* EXERCISE 3
-    Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
-    Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
-*/
+// /* EXERCISE 3
+//     Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+//     Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+// */
  
 
-let splitMe = (theFuture) {
-return "the", "Future" 
+function splitMe = (theFuture) {
+return ["the", "Future"] 
 
 console.log(splitMe)
 
 
-/* EXERCISE 4
-    Write a function called deleteOne which receives a string and a boolean as parameters.
-    If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
-*/
+// /* EXERCISE 4
+//     Write a function called deleteOne which receives a string and a boolean as parameters.
+//     If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+// */
 
-let deleteOne = { 
-             "cup" 
-             true,
+function deleteOne =  (cup,chain) {
+             if (chain = true)
+             return cup;
+
 console.log(deleteOne)
 
 
-/* EXERCISE 5
-   Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
-   Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
-*/
+// /* EXERCISE 5
+//    Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
+//    Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
+// */
 
 let onlyLetters = "made for 1"
 
@@ -131,14 +134,19 @@ let onlyLetters = made for (1)
 console.log(onlyletters)
 }
 
-/* EXERCISE 6
-   Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
-*/
+// /* EXERCISE 6
+//    Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
+// */
 
 function isThisAnEmail = "welcome"
 
-if isThisAnEmail (welcome)
+if isThisAnEmail (welcome) {
+    return 
 
-/* EXERCISE 7
-   Write a function called whatDayIsIt that should return the current day of the week.
-*/
+
+// /* EXERCISE 7
+//    Write a function called whatDayIsIt that should return the current day of the week.
+// */
+
+function whatDayIsIt (monday) {
+    return (monday)
